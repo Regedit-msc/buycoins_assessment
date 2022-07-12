1c12f412ed6114fc96c1208201f8aa03edaf9627
mide <efusanyaae@gmail.com>
2022-07-12 14:10:45 +0100

    update

--- src/fetch.ts
+++ src/fetch.ts
@@ -1,21 +1,21 @@
-const { fetch: originalFetch } = window;
-window.fetch = async (...args) => {
-  const loader = document.createElement('div');
-  loader.classList.add('loading');
-  document.body.style.position = 'relative';
-  document.body.appendChild(loader);
-  try {
-    const response = await originalFetch(...args);
-    document.body.removeChild(loader);
-    document.body.style.position = 'static';
-    if (!response.ok) {
-      throw response;
-    }
-    return response;
-  } catch (e) {
-    document.body.removeChild(loader);
-    document.body.style.position = 'static';
-    return new Response();
-  }
-};
+// const { fetch: originalFetch } = window;
+// window.fetch = async (...args) => {
+//   const loader = document.createElement('div');
+//   loader.classList.add('loading');
+//   document.body.style.position = 'relative';
+//   document.body.appendChild(loader);
+//   try {
+//     const response = await originalFetch(...args);
+//     document.body.removeChild(loader);
+//     document.body.style.position = 'static';
+//     if (!response.ok) {
+//       throw response;
+//     }
+//     return response;
+//   } catch (e) {
+//     document.body.removeChild(loader);
+//     document.body.style.position = 'static';
+//     return new Response();
+//   }
+// };
 export { };
