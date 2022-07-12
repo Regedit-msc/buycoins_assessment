1b176c6725ced1f6d7d4d72035e68bd7eab57011
mide <efusanyaae@gmail.com>
2022-07-12 13:52:12 +0100

    update

--- src/pages/base/index.tsx
+++ src/pages/base/index.tsx
@@ -1,6 +1,6 @@
 import { useState, useMemo, useEffect } from "react";
 import { Column, FullColumn, Input } from "src/components";
-import { useLazyQuery } from "@apollo/client";
+import { useQuery } from "@apollo/client";
 import { GET_TRANSACTIONS, mockData } from "src/constants";
 import { Filter } from "./components/filter";
 
@@ -10,34 +10,18 @@ import { Filter } from "./components/filter";
 const Base = () => {
     const [filterText, setFilterText] = useState("");
     const memoizedFilterText = useMemo(() => filterText, [filterText]);
-    const [initialItems, setInitialItems] = useState<typeof mockData>();
-    const [getData, { loading }] = useLazyQuery(GET_TRANSACTIONS);
-
-    useEffect(() => {
-        const items = localStorage.getItem("ITEMS");
-        if (items && items !== "undefined") {
-            setInitialItems(JSON.parse(items));
-        }
-        (async () => {
-            const { data } = await getData();
-            setInitialItems(data?.getTransactions?.data);
-            localStorage.setItem("ITEMS", JSON.stringify(data?.getTransactions?.data,));
-        })();
-
-    }, []);
-
-
+    const { loading, data } = useQuery(GET_TRANSACTIONS);
 
     return <Column width="100%" align="center" justify="center" height="100%">
         <FullColumn gap={2} padding="2rem" width="40%" align="center" justify="center" wrap="wrap" >
-            {loading && !initialItems && <Column align="center" justify="center" width="100%" height="100vh">Fetching data 🙂...</Column>}
+            {loading && <Column align="center" justify="center" width="100%" height="100vh">Fetching data 🙂...</Column>}
             {
-                initialItems && <>
+                data && !loading && <>
 
                     <Input placeholder="Search items" label="Search items"
                         onChange={(e) => setFilterText(e.target.value)}
                     />
-                    <Filter {...{ filterText: memoizedFilterText, initialItems, setFilter: setFilterText }} />
+                    <Filter {...{ filterText: memoizedFilterText, initialItems: data?.getTransactions?.data, setFilter: setFilterText }} />
                 </>
             }
         </FullColumn>
